cd1f5f524058e89b6e159163d7af04de59078832
Saturnyoshi <actuallysaturnyoshi@gmail.com>
2022-02-25 16:46:21 -0500

    fixes to make the project compile

--- GMLScriptEnv/LuaScriptEnv.cpp
+++ GMLScriptEnv/LuaScriptEnv.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "LuaScriptEnv.h"
-#include <lua.h>
+/*#include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 #include "DebugTools.h"
@@ -9,17 +9,18 @@ void ShowError(lua_State* state) {
     const char* message = lua_tostring(state, -1);
     ShowMessage(message);
     lua_pop(state, 1);
-}
+}*/
 
 namespace LuaScriptEnv {
-    lua_State* state;
+//    lua_State* state;
     void startWithDefaultFile(const char* filename) {
+/*
         state = luaL_newstate();
         luaL_openlibs(state);
         int result = luaL_dofile(state, filename);
         if (result != LUA_OK) {
             ShowError(state);
         }
+*/
     }
-
 }
